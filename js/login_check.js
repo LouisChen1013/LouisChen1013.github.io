88ec8e684fa4ed560ee332d45ad21778c94f51c9
lin1013 <chenhonglin1013@gmail.com>
2019-04-10 17:12:42 -0700

    w

--- js/login_check.js
+++
@@ -1,8 +0,0 @@
-firebase.auth().onAuthStateChanged(function (user) {
-    if (user) {
-        alert(user.email);
-        console.log(user.toJSON());
-    } else {
-        alert("user not signed in");
-    }
-});
--- js/time.js
+++
@@ -1,9 +0,0 @@
-var display = setInterval(function () {
-    Time();
-}, 0);
-
-function Time() {
-    var date = new Date();
-    var time = date.toLocaleTimeString();
-    document.getElementById("clock").innerHTML = time;
-}
